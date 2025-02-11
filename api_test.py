de5c77c790b5c11ce2068426dd0c08e955e2b416
Panflan <107771496+Panflan@users.noreply.github.com>
2025-02-11 01:50:05 -0600

    added .env

--- api_test.py
+++ api_test.py
@@ -1,60 +1,20 @@
-#import requests
-
-#response = requests.get("https://randomfox.ca/floof")
-
-#print(response.status_code) #shows either 200 or 'OK'
-#print(response.text) #shows things you can do
-#print(response.json()) #shows actual things you can do with the 'dictionary'
-
-#fox = response.json()
-#print(fox['image'])
-
-'''
-base_url = "https://pokeapi.co/api/v2/"
-
-def get_pokemon_info(name):
-    url = f"{base_url}/pokemon/{name}"
-    response = requests.get(url)
-
-    if response.status_code == 200:
-        pokemon_data = response.json()
-        return pokemon_data
-
-    else:
-        print(f"Failed to retrieve data {response.status_code}")
-
-
-pokemon_name = "typhlosion"
-pokemon_info = get_pokemon_info(pokemon_name)
-
-if pokemon_info:
-    print(f"Name: {pokemon_info['name']}")
-    print(f"ID: {pokemon_info['id']}")
-    print(f"Height: {pokemon_info['height']}")
-    print(f"Weight: {pokemon_info['weight']}")
-'''
-
+from dotenv import load_dotenv
+import os
 import google.generativeai as genai
+
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")
 def response_feedback(text):
-    genai.configure(api_key="AIzaSyDvtnxy-N72AM0LDvbWoBiTK_Rw5CXjTec")
+    genai.configure(api_key=API_KEY)
     model = genai.GenerativeModel("gemini-1.5-flash")
-    response = model.generate_content("Explain how AI works")
+    response = model.generate_content("What is 2+2 equal to?")
     print(response.text)
     pass
-
-genai.configure(api_key="AIzaSyDvtnxy-N72AM0LDvbWoBiTK_Rw5CXjTec")
-model = genai.GenerativeModel("gemini-1.5-flash")
-response = model.generate_content("")
-print(response.text)
-
-
-
-
 """
 make it so that it inputs speetch and 
 then outputs it in around 100 words
 
-
-using deepspeech
+using Whisper
 """
 
